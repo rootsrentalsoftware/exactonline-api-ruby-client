33aae886519210db9d306830479d46a0eff0c05b
Marthyn <Marthyn@live.nl>
2016-10-18 09:54:55 +0200

    Fix rubocop issue

--- lib/elmas/resources/sales_item_prices.rb
+++ lib/elmas/resources/sales_item_prices.rb
@@ -9,30 +9,12 @@ module Elmas
     def mandatory_attributes
     end
 
+
     def other_attributes
       [
-        :account,
-        :account_name,
-        :created,
-        :creator,
-        :creator_full_name,
-        :currency,
-        :default_item_unit,
-        :default_item_unit_description,
-        :division,
-        :end_date,
-        :item,
-        :item_code,
-        :item_description,
-        :modified,
-        :modifier,
-        :modifier_full_name,
-        :number_of_items_per_unit,
-        :price,
-        :quantity,
-        :start_date,
-        :unit,
-        :unit_description
+        :account, :account_name, :created, :creator, :creator_full_name, :currency, :default_item_unit, :default_item_unit_description,
+        :division, :end_date, :item, :item_code, :item_description, :modified, :modifier, :modifier_full_name, :number_of_items_per_unit,
+        :price, :quantity, :start_date, :unit, :unit_description
       ]
     end
   end
