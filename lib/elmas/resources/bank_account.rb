02881083089916738032a1bbb58ba4dbc2a8a44d
Peter Duijnstee <peter@commuun.nl>
2017-06-13 15:07:36 +0200

    Rubocop cleanup

--- lib/elmas/resources/bank_account.rb
+++ lib/elmas/resources/bank_account.rb
@@ -17,7 +17,22 @@ module Elmas
 
     # https://start.exactonline.nl/docs/HlpRestAPIResourcesDetails.aspx?name=CRMAccounts
     def other_attributes # rubocop:disable Metrics/MethodLength
-      [:id, :account, :account_name, :bank, :bank_account, :bank_description, :bank_name, :BIC_code, :description, :division, :format, :IBAN, :type, :type_description ]
+      [
+        :id,
+        :account,
+        :account_name,
+        :bank,
+        :bank_account,
+        :bank_description,
+        :bank_name,
+        :BIC_code,
+        :description,
+        :division,
+        :format,
+        :IBAN,
+        :type,
+        :type_description
+      ]
     end
   end
 end
