b804ebcd0de46591d65ec600f1877b4002f51a8a
Michiel Verkoijen <michielverkoijen@voormedia.com>
2017-11-01 15:41:11 +0100

    Add division class usefull for retrieving all available divisions

--- lib/elmas.rb
+++ lib/elmas.rb
@@ -44,6 +44,7 @@ require "elmas/resources/vat_code"
 require "elmas/resources/general_journal_entry"
 require "elmas/resources/general_journal_entry_line"
 require "elmas/resources/payment_condition"
+require "elmas/resources/division"
 
 module Elmas
   extend Config
