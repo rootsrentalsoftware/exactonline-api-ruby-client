76821371e86b82eba50b80972283e463258cea92
Charles Hicks <carl.hicks@gmail.com>
2018-01-07 12:47:39 -0800

    Override find_all method to correct API inconsistency

--- lib/elmas/resources/goods_delivery_line.rb
+++ lib/elmas/resources/goods_delivery_line.rb
@@ -4,6 +4,16 @@ module Elmas
     include Elmas::Resource
     include Elmas::SharedSalesAttributes
 
+    # For some reason the Exact API for GoodsDelivery requires us to specify
+    # the fields we want returned.  This isn't required for other calls.  :/
+    # We get around this by specifying a wildcard on the $select param.
+    def find_all(options = {})
+      @order_by = options[:order_by]
+      @select = options[:select] ||= ['*']
+      response = get(uri([:order, :select]))
+      response.results if response
+    end
+
     def base_path
       "salesorder/GoodsDeliveryLines"
     end
@@ -18,7 +28,7 @@ module Elmas
           :quantity_delivered, :quantity_ordered,
           :sales_order_line_id, :sales_order_line_number,
           :serial_numbers, :storage_location,
-          :tracking_number, :unit_code 
+          :tracking_number, :unit_code
         ],
         :<<
       )
