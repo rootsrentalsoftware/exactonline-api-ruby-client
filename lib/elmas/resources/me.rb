f6da87fef85bdf0c297be0fd7e5b47a689459245
Sander Tuin <git@sander.garden>
2022-09-12 14:23:44 +0200

    Update me.rb

--- lib/elmas/resources/me.rb
+++ lib/elmas/resources/me.rb
@@ -5,7 +5,7 @@ module Elmas
     include Elmas::Resource
 
     def base_path
-      "/Me"
+      "current/Me"
     end
 
     def mandatory_attributes
@@ -15,7 +15,10 @@ module Elmas
     def other_attributes
       %i[
         current_division division_customer division_customer_code current_division full_name user_ID user_name language_code
-        legislation email title first_name last_name gender language phone
+        legislation email title first_name last_name gender language phone division_customer_name division_customer_vat_number
+        division_customer_siret_number dossier_division accounting_division is_my_firm_portal_user is_client_user picture_url
+        thumbnail_picture thumbnail_picture_format initials middle_name nationality phone_extension mobile server_time
+        server_utc_offset employee_ID
       ]
     end
   end
